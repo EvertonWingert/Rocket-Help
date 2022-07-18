12f1429428340ce26981515ccb2b5c09207ab1c3
Everton <54149497+EvertonWingert@users.noreply.github.com>
2022-07-18 18:27:26 -0300

    add routes

---
+++ lib/app/routes/app_routes.dart
@@ -0,0 +1,10 @@
+import '../pages/sign_in_page.dart';
+import '../pages/splash_page.dart';
+import 'app_pages.dart';
+
+class AppPages {
+  static final routes = {
+    Routes.splash: (_) => const SplashPage(),
+    Routes.signin: (_) => const SignInPage(),
+  };
+}
